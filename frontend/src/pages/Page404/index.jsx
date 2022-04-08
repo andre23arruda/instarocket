81278b965c5d288e02240b82aa9d6faff122af9a
Andre <andre23arruda@gmail.com>
2022-04-08 09:39:25 -0400

    feat: Add 404 page

---
+++ frontend/src/pages/Page404/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+// utils
+import { title } from '../../utils'
+
+
+// images and styles
+import './Page404.scss'
+
+
+function Page404() {
+    title('Página não encontrada')
+
+    return (
+        <div className="container-404">
+            <h1>
+                Ooops! <br />
+                Parece que essa página não existe
+            </h1>
+
+            <Link className="back-link" to='/'>
+                Voltar
+            </Link>
+        </div>
+    )
+}
+
+export default Page404
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,20 +1,25 @@
 import React from 'react'
-import { BrowserRouter,  Route } from 'react-router-dom'
+import { BrowserRouter,  Route, Switch } from 'react-router-dom'
 
 // pages
 import Feed from './pages/Feed'
 import Login from './pages/Login'
 import NewPost from './pages/NewPost'
+import Page404 from './pages/Page404'
 import Signup from './pages/Signup'
 
 
 function Routes(){
     return (
         <BrowserRouter>
-            <Route exact path='/' component={ Feed } />
-            <Route exact path='/login' component={ Login } />
-            <Route exact path='/signup' component={ Signup } />
-            <Route exact path='/new' component={ NewPost } />
+            <Switch>
+                <Route exact path='/' component={ Feed } />
+                <Route exact path='/login' component={ Login } />
+                <Route exact path='/signup' component={ Signup } />
+                <Route exact path='/new' component={ NewPost } />
+
+                <Route component={ Page404 } />
+            </Switch>
         </BrowserRouter>
     )
 }
